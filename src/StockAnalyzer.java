1a6a358fed67c40756242ce1197a6b79efcdfabc
Akhil Thata <athata1@github.com>
2021-09-16 17:07:49 -0400

    added java template

--- src/StockAnalyzer.java
+++ src/StockAnalyzer.java
@@ -1,2 +1,26 @@
-public class StockAnalyzer {
+import java.awt.*;
+import java.awt.font.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.*;
+import java.util.*;
+
+public class StockAnalyzer extends JComponent {
+    public StockAnalyzer()
+    {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+    }
+    public static void main(String[] args)
+    {
+    }
 }

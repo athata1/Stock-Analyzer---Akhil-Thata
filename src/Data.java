8b2d65016fe3f78f078126814286609c83f148ca
Akhil Thata <akhil.thata@gmail.com>
2021-09-18 17:50:45 -0400

    Final Project

--- src/Data.java
+++ src/Data.java
@@ -5,13 +5,17 @@ public class Data {
     private int month;
     private int year;
     static double maxCloseValue = 0;
-    public Data(String[] data)
+    public Data(String[] data,String mode)
     {
         date = data[0].split("-");
         year = Integer.parseInt(date[0]);
         month = Integer.parseInt(date[1]);
         day = Integer.parseInt(date[2]);
         closeValue = Double.parseDouble(data[4]);
+        if (mode.toLowerCase().equals("log"))
+        {
+            closeValue = Math.log(closeValue);
+        }
         maxCloseValue = Math.max(maxCloseValue,closeValue);
     }
     public String[] getDate()
--- src/StockAnalyzer.java
+++ src/StockAnalyzer.java
@@ -1,14 +1,14 @@
 import java.awt.*;
 import java.awt.font.*;
 import java.awt.geom.Rectangle2D;
-import java.io.File;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StockAnalyzer extends JPanel {
     
@@ -17,14 +17,15 @@ public class StockAnalyzer extends JPanel {
     private long time2;
     private long time1;
     private BufferedReader reader;
-    ArrayList<Data> data;
+    private ArrayList<Data> data;
     private double ratio;
+    private TreeMap<Integer,Color> yearMap;
+    private String mode;
     public StockAnalyzer()
     {
-        //Load Stock Data
-        time1 = 1600441349;
+        getValues();
         time2 = System.currentTimeMillis();
-        ticker = "AMZN";
+        //Load Stock Data
         stockURL = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + time1 + "&period2=" + time2 + "&interval=1d&events=history&includeAdjustedClose=true";
         try {
             URL url = new URL(stockURL);
@@ -42,7 +43,7 @@ public class StockAnalyzer extends JPanel {
             String s = "";
             while ((s=reader.readLine()) != null)
             {
-                data.add(new Data(s.split(",")));
+                data.add(new Data(s.split(","),mode));
             }
         }
         catch (Exception e)
@@ -50,6 +51,38 @@ public class StockAnalyzer extends JPanel {
             System.out.println("Error while reading data");
         }
         ratio = 400.0/Data.maxCloseValue;
+        yearMap = new TreeMap<Integer,Color>();
+    }
+    public void getValues()
+    {
+        Pattern pattern = Pattern.compile("(0[0-9]||1[0-2])/([0-2][0-9]||3[0-1])/((19|20)\\d\\d)");
+        Matcher matcher;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Ticker:");
+        String date;
+        ticker = scan.nextLine().toUpperCase();
+
+        //Get date1
+        do {
+            System.out.println("\nEnter start date of values (mm/dd/yyyy)");
+            date = scan.nextLine();
+            matcher = pattern.matcher(date);
+        }
+        while(!matcher.matches());
+        String[] dateData = date.split("/");
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.MONTH, Integer.parseInt(dateData[0]));
+        c.set(Calendar.YEAR, Integer.parseInt(dateData[2]));
+        c.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateData[1]));
+        time1 = c.getTimeInMillis()/1000;
+
+        do
+        {
+            System.out.println("Set Mode: regular, log, or gradient");
+            mode = scan.nextLine().toLowerCase();
+        }
+        while (!mode.equals("regular") && !mode.equals("log") && !mode.equals("gradient"));
     }
     @Override
     public void paintComponent(Graphics g)
@@ -63,7 +96,6 @@ public class StockAnalyzer extends JPanel {
 
         //Draw Title
         Font font = new Font("Courier New", Font.BOLD, 30);
-        //TODO draw title
         centerText(0,-500,font,35*12,100,ticker + " Data",g);
 
         //Label X axis
@@ -97,24 +129,88 @@ public class StockAnalyzer extends JPanel {
         {
             g.drawString(yAxisName.substring(i,i+1),-150,-350 + 29*i);
         }
-        
-        //Display close values
-        int prevX = -1;
-        int prevY = -1;
-        for (Data d: data)
-        {
-            int day = d.getDay();
-            int month = d.getMonth();
-            int year = d.getYear();
-            int xCoord = (int)Math.round((month-1)*35 + 1.0*day/getDaysInMonth(month)*35);
-            int yCoord = (int)Math.round(-d.getCloseVal()*ratio);
-            fillCircle(xCoord,yCoord,1,g);
-            if (prevX != -1 && Math.abs(prevX - xCoord) <= 5)
-            {
-                g.drawLine(prevX,prevY,xCoord,yCoord);
+
+        if (!mode.equals("gradient")) {
+            //Display close values
+            int prevX = -1;
+            int prevY = -1;
+            for (Data d : data) {
+                int day = d.getDay();
+                int month = d.getMonth();
+                int year = d.getYear();
+                int xCoord = (int) Math.round((month - 1) * 35 + 1.0 * day / getDaysInMonth(month) * 35);
+                int yCoord = (int) Math.round(-d.getCloseVal() * ratio);
+                fillCircle(xCoord, yCoord, 1, g);
+                //Change color of line
+                if (yearMap.containsKey(year))
+                    g.setColor(yearMap.get(year));
+                else
+                    yearMap.put(year, new Color((int) (Math.random() * 240), (int) (Math.random() * 240), (int) (Math.random() * 240)));
+
+                if (prevX != -1 && Math.abs(prevX - xCoord) <= 5) {
+                    g.drawLine(prevX, prevY, xCoord, yCoord);
+                }
+                prevX = xCoord;
+                prevY = yCoord;
             }
-            prevX = xCoord;
-            prevY = yCoord;
+
+            //Draw Key
+            g.setColor(Color.BLACK);
+            g.drawString("Key:", 35 * 12 + 50, -375);
+            g.setFont(new Font("Courier New", Font.BOLD, 15));
+            int i = 0;
+            for (Map.Entry<Integer, Color> entry : yearMap.entrySet()) {
+                int key = entry.getKey();
+                Color c = entry.getValue();
+                g.setColor(c);
+                g.fillRect(35 * 12 + 50, -350 + 25 * i, 15, 15);
+                g.drawString(key + "", 35 * 12 + 70, -335 + 25 * i);
+                i++;
+            }
+
+            g.setColor(Color.BLACK);
+            g.drawRect(35 * 12 + 40, -400, 100, i * 25 + 50);
+        }
+        else
+        {
+            TreeMap<String,String> map = new TreeMap<String,String>();
+            int prevX = -1;
+            int prevY = -1;
+            double slope = -1;
+            for (Data d : data) {
+                int day = d.getDay();
+                int month = d.getMonth();
+                int xCoord = (int) Math.round((month - 1) * 35 + 1.0 * day / getDaysInMonth(month) * 35);
+                int yCoord = (int) Math.round(-d.getCloseVal() * ratio);
+
+                if (prevX != -1)
+                {
+                    slope = 1.0*(yCoord - prevY) / (1.0 * xCoord - prevX);
+                    char ch = (slope >= 0) ? 'p' : 'n';
+                    if (map.containsKey(month + "/" + day))
+                    {
+                        String s = map.get(month + "/" + day);
+                        map.put(month + "/" + day, s + ch);
+                    }
+                    else
+                        map.put(month + "/" + day,ch + "");
+                }
+
+                prevX = xCoord;
+                prevY = yCoord;
+            }
+            for (Map.Entry<String,String> entry: map.entrySet())
+            {
+                String str = entry.getValue();
+                int denominator = str.length();
+                int numerator = denominator - str.replaceAll("p","").length();
+                String[] date = entry.getKey().split("/");
+                g.setColor(new Color((int)((1-1.0*numerator/denominator)*255),(int)(1.0*numerator/denominator*255),0));
+                int day = Integer.parseInt(date[1]);
+                int month = Integer.parseInt(date[0]);
+                int xCoord = (int) Math.round((month - 1) * 35 + 1.0 * day / getDaysInMonth(month) * 35);
+                g.drawLine(xCoord,0,xCoord,-400);
+            }
         }
     }
     public void centerText(int x, int y, Font font, int width, int height, String s, Graphics g)
